d7f3cd02f13f758eb6324a800a8d8ef9a1b7e213
CodeWizard <codewizard001@gmail.com>
2020-10-27 06:10:21 +0530

    Functional Mouse Controller

--- .built-in.a.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/wizard/Work/osTheory/built-in.a := rm -f /home/wizard/Work/osTheory/built-in.a; ar cDPrST /home/wizard/Work/osTheory/built-in.a 
--- .keylogger.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/wizard/Work/osTheory/keylogger.mod := { echo  /home/wizard/Work/osTheory/keylogger.o; echo; } > /home/wizard/Work/osTheory/keylogger.mod
